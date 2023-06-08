c7c43c7134c0eb13be0dcc189651151c5fd0f36b
kiram <kira.morozovkirill.2018.aprel@gmail.com>
2023-06-08 15:18:31 +0300

    init

--- server/src/dbpool.ts
+++ server/src/dbpool.ts
@@ -1,13 +1,15 @@
 import {Pool} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbPool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '1Kimonojoom1',
-    port: 5432,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
 });
-
 
 
 export default dbPool;
